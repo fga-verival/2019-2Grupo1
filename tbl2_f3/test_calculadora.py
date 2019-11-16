e75a8f08c2821036171e0b005219c49fd0b584e7
Mateus Oliveira <o.mateus.p@gmail.com>
2019-11-16 14:58:32 -0300

    Sucesso nos testes por parametrisacao Co-authored-by: André Bargas <andrebargas@gmail.com> Co-authored-by: Lucas Machado <lmlucasmachadom@gmail.com> Co-authored-by: Brian Lui  <brianlui2387@gmail.com> Co-authored-by: Tâmara Barbosa  <tamara-bt@hotmail.com>

--- tbl2_f3/test_calculadora.py
+++ tbl2_f3/test_calculadora.py
@@ -24,7 +24,7 @@ def calcula_imposto(n_dias, valor_inicial, taxa_anual):
 
     imp_pago = redimento_bruto * tipo_tributacao
 
-    redimento_liquido = (redimento_bruto - imp_pago) / valor_inicial
+    redimento_liquido = ((redimento_bruto - imp_pago) / valor_inicial)*100
 
     return [round(redimento_bruto, 2), round(imp_pago, 2), round(redimento_liquido, 2)]
 
@@ -33,12 +33,24 @@ class TesteCalculaCDB(unittest.TestCase):
 
     def test_redimento_bruto(self):
         self.assertEqual(calcula_imposto(60, 1000, 0.085)[0], 13.41)
+        self.assertEqual(calcula_imposto(120, 500, 0.08)[0], 12.65)
+        self.assertEqual(calcula_imposto(240, 3000, 0.09)[0], 170.01)
+        self.assertEqual(calcula_imposto(270, 2000, 0.085)[0], 120.71)
+        self.assertEqual(calcula_imposto(390, 100, 0.075)[0], 7.73)
 
     def test_imp_pago(self):
         self.assertEqual(calcula_imposto(60, 1000, 0.085)[1], 3.02)
+        self.assertEqual(calcula_imposto(120, 500, 0.08)[1], 2.85)
+        self.assertEqual(calcula_imposto(240, 3000, 0.09)[1], 34.0)
+        self.assertEqual(calcula_imposto(270, 2000, 0.085)[1], 24.14)
+        self.assertEqual(calcula_imposto(390, 100, 0.075)[1], 1.35)
 
     def test_redimento_liquido(self):
-        self.assertEqual(calcula_imposto(60, 1000, 0.085)[2], 0.01)
+        self.assertEqual(calcula_imposto(60, 1000, 0.085)[2], 1.04)
+        self.assertEqual(calcula_imposto(120, 500, 0.08)[2], 1.96)
+        self.assertEqual(calcula_imposto(240, 3000, 0.09)[2], 4.53)
+        self.assertEqual(calcula_imposto(270, 2000, 0.085)[2], 4.83)
+        self.assertEqual(calcula_imposto(390, 100, 0.075)[2], 6.38)
 
 
 if __name__ == "__main__":
